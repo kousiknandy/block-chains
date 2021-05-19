9c3ed2fe664d11eb9e09136c50cac78201add793
Kousik Nandy <kousiknandy@gmail.com>
2021-05-19 20:16:13 +0530

    simplest solution, broadcast a superset every round

--- cryptocurrency/bitcoincryptocurrency/assignment2/CompliantNode.java
+++ cryptocurrency/bitcoincryptocurrency/assignment2/CompliantNode.java
@@ -4,24 +4,48 @@ import java.util.Set;
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private int followees[];
+    private Set<Transaction> initialTxns;
+    private double p_graph, p_malicious, p_txDistribution;
+    private int numRounds, currentRound;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+	this.p_malicious = p_malicious;
+	this.p_txDistribution = p_txDistribution;
+	this.numRounds = numRounds;
+	this.currentRound = 0;
     }
 
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
+	this.followees = new int[followees.length];
+	int i = 0;
+	for (boolean f: followees) {
+	    this.followees[i] = f ? 1 : 0;
+	    i++;
+	}
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
         // IMPLEMENT THIS
+	System.out.println(this.hashCode() + " setPendingTransaction " + pendingTransactions);
+	this.initialTxns = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
-	return null;
+	System.out.println(this.hashCode() + " sendToFollowers ");
+
+	return this.initialTxns;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+	this.currentRound++;
+	if (this.currentRound < this.numRounds - 1) {
+	    for (Candidate c: candidates) {
+		this.initialTxns.add(c.tx);
+	    }
+	}
     }
 }
