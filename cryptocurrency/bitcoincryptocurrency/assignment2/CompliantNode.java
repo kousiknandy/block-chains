db9187c713496fb91fa2aa641f3f69523ebbdfa5
Kousik Nandy <kousiknandy@gmail.com>
2021-05-20 14:18:45 +0530

    Freeze transactions on Rounds-2 iteration

--- cryptocurrency/bitcoincryptocurrency/assignment2/CompliantNode.java
+++ cryptocurrency/bitcoincryptocurrency/assignment2/CompliantNode.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
+
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
@@ -12,40 +14,28 @@ public class CompliantNode implements Node {
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         this.p_graph = p_graph;
 	this.p_malicious = p_malicious;
-	this.p_txDistribution = p_txDistribution;
-	this.numRounds = numRounds;
-	this.currentRound = 0;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+        this.currentRound = 0;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
-	this.followees = new int[followees.length];
-	int i = 0;
-	for (boolean f: followees) {
-	    this.followees[i] = f ? 1 : 0;
-	    i++;
-	}
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
-	System.out.println(this.hashCode() + " setPendingTransaction " + pendingTransactions);
-	this.initialTxns = pendingTransactions;
+        this.initialTxns = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
-	System.out.println(this.hashCode() + " sendToFollowers ");
-
-	return this.initialTxns;
+        return this.initialTxns;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-	this.currentRound++;
-	if (this.currentRound < this.numRounds - 1) {
-	    for (Candidate c: candidates) {
-		this.initialTxns.add(c.tx);
-	    }
-	}
+        this.currentRound++;
+        if (this.currentRound < this.numRounds - 2) {
+            for (Candidate c: candidates) {
+                this.initialTxns.add(c.tx);
+            }
+        }
     }
 }
